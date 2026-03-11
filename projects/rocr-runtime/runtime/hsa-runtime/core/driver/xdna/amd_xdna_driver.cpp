2f43f0596f604c8801d43e582c39edd0a58167c5
Yiannis Papadopoulos <Yiannis.Papadopoulos@amd.com>
2026-03-11 16:20:21 -0400

    rocr/aie: Additional check for allocation from dev heap. Documenting the differences between dev heap and typical SVM.

--- projects/rocr-runtime/runtime/hsa-runtime/core/driver/xdna/amd_xdna_driver.cpp
+++ projects/rocr-runtime/runtime/hsa-runtime/core/driver/xdna/amd_xdna_driver.cpp
@@ -3,7 +3,7 @@
 // The University of Illinois/NCSA
 // Open Source License (NCSA)
 //
-// Copyright (c) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2024-2026, Advanced Micro Devices, Inc. All rights reserved.
 //
 // Developed by:
 //
@@ -318,6 +318,13 @@ XdnaDriver::AllocateMemory(const core::MemoryRegion &mem_region,
   if (use_bo_shmem) {
     create_bo_args.type = AMDXDNA_BO_SHMEM;
   } else {
+    // While this is already checked in MemoryRegion::AllocateImpl, the max size is
+    // MemoryRegion::max_sysmem_alloc_size_ for HSA_HEAPTYPE_DEVICE_SVM which is incorrect
+    // for dev heap.
+    if (size > dev_heap_size) {
+      return HSA_STATUS_ERROR_INVALID_ALLOCATION;
+    }
+
     create_bo_args.type = AMDXDNA_BO_DEV;
   }
 
