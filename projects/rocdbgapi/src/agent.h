ROCm/ROCdbgapi

Move the apertures configurations to the agent
Since the apertures configurations are agent dependent, move the
apertures configurations from the generic address space to the agent.
Use the agent's os_info to set the memory regions.

src/agent.h
@@ -33,6 +33,7 @@
 namespace amd::dbgapi
 {
 
+class address_space_t;
 class architecture_t;
 class process_t;
 class watchpoint_t;
@@ -41,12 +42,21 @@ class watchpoint_t;
 
 class agent_t : public detail::handle_object<amd_dbgapi_agent_id_t>
 {
+public:
+  struct aperture_t
+  {
+    amd_dbgapi_global_address_t base;
+    amd_dbgapi_global_address_t limit;
+    const address_space_t &address_space;
+  };
+
 private:
   os_agent_info_t const m_os_agent_info;
   os_exception_mask_t m_exceptions{ os_exception_mask_t::none };
   epoch_t m_mark{ 0 };
 
   std::vector<const watchpoint_t *> m_watchpoints;
+  const std::vector<aperture_t> m_apertures;
   const architecture_t *const m_architecture;
   process_t &m_process;
 
@@ -118,6 +128,8 @@ public:
 
   void get_info (amd_dbgapi_agent_info_t query, size_t value_size,
                  void *value) const;
+
+  const auto &apertures () const { return m_apertures; }
 
   const architecture_t *architecture () const { return m_architecture; }
 

Praneeth-Vallem/rocm-systems

Change-Id: I476d83fa294fbc5b8d7f0ff20eb3f41b71d60fe1

projects/rocdbgapi/src/agent.h
@@ -33,6 +33,7 @@
 namespace amd::dbgapi
 {
 
+class address_space_t;
 class architecture_t;
 class process_t;
 class watchpoint_t;
@@ -41,12 +42,21 @@ class watchpoint_t;
 
 class agent_t : public detail::handle_object<amd_dbgapi_agent_id_t>
 {
+public:
+  struct aperture_t
+  {
+    amd_dbgapi_segment_address_t base;
+    amd_dbgapi_segment_address_t limit;
+    const address_space_t &address_space;
+  };
+
 private:
   os_agent_info_t const m_os_agent_info;
   os_exception_mask_t m_exceptions{ os_exception_mask_t::none };
   epoch_t m_mark{ 0 };
 
   std::vector<const watchpoint_t *> m_watchpoints;
+  const std::vector<aperture_t> m_apertures;
   const architecture_t *const m_architecture;
   process_t &m_process;
 
@@ -118,6 +128,8 @@ public:
 
   void get_info (amd_dbgapi_agent_info_t query, size_t value_size,
                  void *value) const;
+
+  const auto &apertures () const { return m_apertures; }
 
   const architecture_t *architecture () const { return m_architecture; }
 